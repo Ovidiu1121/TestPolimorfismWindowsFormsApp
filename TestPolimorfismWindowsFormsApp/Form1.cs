57e66e30e804602a00a0d0b1bdf8bd6e72c8c5cf
Ovidiu Gusteritan <gusteritanovidiu@gmail.com>
2023-03-14 20:51:28 +0200

    first commit

---
+++ TestPolimorfismWindowsFormsApp/Form1.cs
@@ -0,0 +1,10 @@
+namespace TestPolimorfismWindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
